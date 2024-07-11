474022b8d44e8470d2c752a124a1622bb5ee7a4a
MindfulLearner <dimajosh1009@gmail.com>
2024-07-11 05:22:45 +0200

    completo

---
+++ js/script.js
@@ -0,0 +1,17 @@
+document.writeln('hello world <br>');
+
+let nomeUtente = prompt("Inserisci il tuo nome"); 
+let cognomeUtente = prompt("Ora il cognome"); 
+let coloreUtente = prompt("Colore preferito?"); 
+
+document.writeln(`Ti chiami: ${nomeUtente}, di cognome fai ${cognomeUtente} e ti piace il colore ${coloreUtente} `);
+
+
+
+
+
+
+
+
+
+
